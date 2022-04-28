dbdcf920321d3ca9f23477399d87c1a3dc747ed2
vitaliy zems <vitalyazems@gmail.com>
2022-04-28 20:06:41 +0600

    Рабочая версия

--- js/main.js
+++ js/main.js
@@ -1,25 +1,22 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 1000},
-    {id: 2, title: 'Mouse', price: 100},
-    {id: 3, title: 'Keyboard', price: 250},
-    {id: 4, title: 'Gamepad', price: 150},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить</button>
-              </div>`;
-};
+const app = new Vue({
+    el: '#app',
+    data: {
+        userSearch: '',
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    // show error component
+                    console.log(error);
+                })
+        },
+    },
+    mounted() {
+        console.log(this);
+    }
+});
 
-const renderProducts = (list) => {
-    const productList = list.map((good) => {
-        return renderProduct(good.title, good.price);
-    });
-    document.querySelector('.products').innerHTML = productList;
-
-    console.log(productList);
-};
-
-renderProducts(products);
